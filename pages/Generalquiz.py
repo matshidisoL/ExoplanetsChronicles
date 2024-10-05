59b374edb1b0dc856e786b60fdae97235efd16de
Topollo Naketsana <120329118+topollonaketsana@users.noreply.github.com>
2024-10-05 20:57:14 +0200

    Update Generalquiz.py

--- pages/Generalquiz.py
+++ pages/Generalquiz.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+
 # Set up the page configuration
 st.set_page_config(page_title="Exoplanet Explorer", layout="wide")
 #Set up page background
